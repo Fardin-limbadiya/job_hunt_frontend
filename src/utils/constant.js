c490fb9312074f24b8bf5ca513aee182cdbfb25c
Ashura <ashuramajestic@gmail.com>
2024-09-25 11:06:43 +0530

    ashura changed constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://jobhuntbackend.netlify.app/api/v1/user";
-export const JOB_API_END_POINT = "https://jobhuntbackend.netlify.app/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://jobhuntbackend.netlify.app/api/v1/application";
-export const COMPANY_API_END_POINT = "https://jobhuntbackend.netlify.app/api/v1/company";
+export const USER_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${process.env.REACT_APP_URL}/api/v1/company`;
